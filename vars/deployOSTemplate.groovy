807c57b51572da93904182087d6cbfb02b877828
gargpriyank <priyank.garg@ibm.com>
2020-09-01 17:59:52 -0500

    Updated deployOSTemplate

--- vars/deployOSTemplate.groovy
+++ vars/deployOSTemplate.groovy
@@ -5,13 +5,13 @@
  *
  * Example:
  * deployOSTemplate(project: 'myproject', templateFullPath: 'template/sample-template.yaml', appName: 'myapp',
- * gitUrl: 'https://', gitBranch: 'master', memLimit: '512Mi', replicas: '2', imageTag: 'latest')
+ * imageRegistryURL: 'docker.io', imageRepo: 'myrepo', memLimit: '512Mi', replicas: '2', imageTag: 'latest')
  *
  * @param project
  * @param templateFullPath
  * @param appName
- * @param gitUrl
- * @param gitBranch
+ * @param imageRegistryURL
+ * @param imageRepo
  * @param memLimit
  * @param replicas
  * @param imageTag
@@ -21,14 +21,15 @@ def call(args) {
     def project = args.project
     def templateFullPath = args.templateFullPath
     def appName = args.appName
-    def gitUrl = args.gitUrl
-    def gitBranch = args.gitBranch
+    def imageRegistryURL = args.imageRegistryURL
+    def imageRepo = args.imageRepo
     def memLimit = args.memLimit ?: '512Mi'
     def replicas = args.replicas ?: '2'
     def imageTag = args.imageTag
 
     sh "oc project $project"
     sh "oc process -f $templateFullPath" +
-            " -pNAME=$appName -pGITHUB_REPO=$gitUrl -pBRANCH=$gitBranch -pMEMORY_LIMIT=$memLimit -pREPLICAS=$replicas -pIMAGE_TAG=$imageTag" +
+            " -pNAME=$appName -pMEMORY_LIMIT=$memLimit -pREPLICAS=$replicas -pIMAGE_REGISTRY=$imageRegistryURL -pIMAGE_REPO=$imageRepo" +
+            " -pIMAGE_TAG=$imageTag" +
             " | oc apply -f -"
 }
