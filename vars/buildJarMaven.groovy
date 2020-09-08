c232109e0c1f19a7d9567fc045fc0d735c95bc4d
gargpriyank <priyank.garg@ibm.com>
2020-09-08 18:23:04 -0500

    Updated code

--- vars/buildJarMaven.groovy
+++ vars/buildJarMaven.groovy
@@ -13,21 +13,11 @@ def call(args) {
     def mvnArgs = args.mvnArgs
     def checkMavenSettings = args.checkMavenSettings ?: false
 
-    if (checkMavenSettings && mvnArgs != null) {
-        echo "1??????"
+    if (checkMavenSettings) {
         configFileProvider([configFile(fileId: 'maven_settings', variable: 'maven_settings')]) {
             sh "-s $maven_settings clean install $mvnArgs"
         }
-    } else if (checkMavenSettings) {
-        echo "2??????"
-        configFileProvider([configFile(fileId: 'maven_settings', variable: 'maven_settings')]) {
-            sh "mvn -s $maven_settings clean install"
-        }
-    } else if (mvnArgs != null) {
-        echo "3??????"
-        sh "mvn clean install $mvnArgs"
     } else {
-        echo "4??????"
         sh "mvn clean install"
     }
 }
